0d359fe768f248a97e946d3dafd425976e06eb96
Linda Paiste <lindapaiste@gmail.com>
2020-09-01 16:45:15 -0500

    point move

--- __tests__/pointMove.test.ts
+++ __tests__/pointMove.test.ts
@@ -1,10 +1,20 @@
-import {IPoint, shiftBy, shiftX, shiftY} from "../src";
+import {IPoint, IRectangle, setX, setY, shiftPointBy, shiftPointX, shiftPointY} from "../src";
 
 test("move points", () => {
     const point: IPoint = {x: 5, y: 10};
 
-    expect( shiftX(point, 20)).toEqual({x: 25, y: 10});
-    expect( shiftY(point, 20)).toEqual({x: 5, y: 30});
-    expect( shiftBy(point, {x: -20, y: -20})).toEqual({x: -15, y: -10});
-    expect( shiftBy(point, point)).toEqual({x: 10, y: 20});
+    expect( shiftPointX(point, 20)).toEqual({x: 25, y: 10});
+    expect( shiftPointY(point, 20)).toEqual({x: 5, y: 30});
+    expect( shiftPointBy(point, {x: -20, y: -20})).toEqual({x: -15, y: -10});
+    expect( shiftPointBy(point, point)).toEqual({x: 10, y: 20});
+})
+
+test("set x y values", () => {
+    const point: IPoint = {x: 5, y: 10};
+    const rect: IRectangle = {x: 5, y: 10, width: 10, height: 20};
+
+    expect( setX(point, 25)).toEqual({x: 25, y: 10});
+    expect( setY(point, 25)).toEqual({x: 5, y: 25});
+    expect( setX(rect, 25)).toEqual({x: 25, y: 10, width: 10, height: 20});
+    expect( setY(rect, 25)).toEqual({x: 5, y: 25, width: 10, height: 20});
 })
--- src/points/move.ts
+++ src/points/move.ts
@@ -1,27 +1,34 @@
 import {IPoint} from "./types";
 
 /**
+ * use spread operator to keep all values of the initial object
+ */
+export const setY = <T extends {y: number}>(obj: T, y: number): T => ({
+    ...obj,
+    y
+});
+export const setX = <T extends {x: number}>(obj: T, x: number): T => ({
+    ...obj,
+    x
+});
+
+/**
  * move just the x value of a point
  */
-export const shiftX = (pos: IPoint, change: number): IPoint => ({
-  x: pos.x + change,
-  y: pos.y,
-})
-
+export const shiftPointX = (pos: IPoint, change: number): IPoint =>
+    setX(pos, pos.x + change);
 
 /**
  * move just the y value of a point
  */
-export const shiftY = (pos: IPoint, change: number): IPoint => ({
-    x: pos.x,
-    y: pos.y + change,
-})
+export const shiftPointY = (pos: IPoint, change: number): IPoint =>
+    setY(pos, pos.y + change);
 
 
 /**
  * move a point based on a change object with x move and y move
  */
-export const shiftBy = (pos: IPoint, change: IPoint): IPoint => ({
+export const shiftPointBy = (pos: IPoint, change: IPoint): IPoint => ({
     x: pos.x + change.x,
     y: pos.y + change.y,
 })
